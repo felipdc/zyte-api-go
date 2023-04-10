f3bc8a984a83980878cce7938057b47c3a3d3219
Felipe <felipetdecarli@gmail.com>
2023-04-10 12:06:41 -0300

    feat: update examples to show actions parameter

--- examples/books.toscrape.go
+++ examples/books.toscrape.go
@@ -11,12 +11,22 @@ func main() {
 
 	// Set options
 	schema := zyte_api_go.RequestSchema{
-		Url:              "http://books.toscrape.com/",
-		HttpResponseBody: true,
+		Url:        "http://books.toscrape.com/",
+		Screenshot: true,
+		Actions: []zyte_api_go.Action{
+			{
+				Action: "click",
+				Selector: zyte_api_go.Selector{
+					Type:  "css",
+					Value: "#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a",
+				},
+			},
+		},
 	}
 
 	options := zyte_api_go.Options{
-		Schema: schema,
+		Schema:         schema,
+		ScreenshotFile: "screenshot.png",
 	}
 
 	// Scrape a website
